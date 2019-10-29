b7b5825c0ea76457e712a4f9675a72a06acbeb53
Student <example@example.com>
2019-10-29 16:15:24 -0700

    update styling, functions of fortune quiz.

--- js/scripts.js
+++ js/scripts.js
@@ -34,19 +34,43 @@ $(function() {
     console.log("Love category: " + love);
 
     var careerSum = 0;
+    var friendSum = 0;
+    var loveSum = 0;
+
 
     for (i = 0; i < friends.length; i++) {
-      careerSum += friends[i];
+      friendSum += friends[i];
     }
 
-    
+    for (i = 0; i < career.length; i++) {
+      careerSum += career[i];
+    }
+
+    for (i = 0; i < love.length; i++) {
+      loveSum += love[i];
+    }
+
+
 
     console.log(careerSum);
 
 
-    if(love === 10 && career === 10 && friends === 10) {
+    if(loveSum === 10 && careerSum === 10 && friendSum === 10) {
       $('.result-bad').show();
-      $('.result-bad card-body').append('<p> Oh child, you need a lot of help! </p>');
+      $('.result-box').empty();
+      $('.box-bad').append('<p>' + 'Oh child, you need a lot of help!' + '</p>');
+    } else if (loveSum >= 6 || careerSum === 6 && friendSum <= 6 ) {
+      $('.result-warn').show();
+      $('.box-warn').empty();
+      $('.box-warn').append('<p>' + 'Be cautious about something' + '</p>');
+    } else if (loveSum >= 2 || careerSum >= 7 || friendSum >=7) {
+      $('.result-good').show();
+      $('.box-good').empty();
+      $('.box-good').append('<p>' + 'Rest easy, you have good fortune coming to you' + '</p>');
+    } else if (loveSum === 0 && careerSum === 0 && friendSum === 0) {
+      $('.result-bad').show();
+      $('.box-bad').empty();
+      $('.box-bad').append('<p>' + 'Oh no, you have no questions for Miss Cleo?' + '</p>');
     }
   });
 
